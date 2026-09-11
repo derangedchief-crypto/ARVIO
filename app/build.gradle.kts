b314e8ed7b03bc8bcb39abb57b916e9b9cac3485
derangedchief-crypto <derangedchief@gmail.com>
2026-09-11 11:22:33 -0400

    Increment versionCode and versionName in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,8 +37,8 @@ android {
         // Fire TV devices can be as low as Android 7.1 (API 25) or lower depending on model/OS.
         minSdk = 23
         targetSdk = 36
-        versionCode = 321
-        versionName = "2.0.007"
+        versionCode = 322
+        versionName = "2.0.1"
         buildConfigField("String", "GITHUB_OWNER", "\"derangedchief-crypto\"")
         buildConfigField("String", "GITHUB_REPO", "\"ARVIO\"")
         buildConfigField("Boolean", "FEATURE_PLUGINS_ENABLED", "false")
